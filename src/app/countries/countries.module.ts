07d04f40eb5036e99c3272dc6859a9107af1983d
saulgame2010 <saulgame2010@gmail.com>
2023-05-30 13:54:13 -0600

    Se agregaron más páginas ahora para rutas hijas

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountriesRoutingModule } from './countries-routing.module';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 
@@ -15,7 +17,8 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
     CountryPageComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    CountriesRoutingModule,
+  ],
 })
 export class CountriesModule { }
